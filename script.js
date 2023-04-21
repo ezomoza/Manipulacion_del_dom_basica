21b74f411cf2377408c494c8315c0b73c2d48909
Emus181 <ema_z18@hotmail.com>
2023-04-21 14:21:28 -0300

    agregar primero archivos de curso

---
+++ script.js
@@ -0,0 +1,23 @@
+const h1 = document.querySelector('h1')
+let input1 = document.querySelector('#calculo1')
+let form = document.querySelector('#form')
+let input2 = document.querySelector('#calculo2')
+const btn = document.querySelector('#btncalcular')
+const presult = document.querySelector('#result')
+
+// form.addEventListener('submit', btnOnClick);
+
+// function btnOnClick(event) {
+//  // console.log(event);
+//  event.preventDefault();
+//  const suma = input1.value + input2.value;
+//  presult.innerText = "Resultado: " + suma;
+// }
+
+h1.addEventListener('copy', btnOnClick);
+
+function btnOnClick() {
+ // console.log(event);
+ // event.preventDefault();
+ alert('virus')
+}
